5700d07abdf542979eb6a4804debc4ca48d5d1d9
Heikkinen Verna <verna.heikkinen@aalto.fi>
2021-08-11 15:45:48 +0300

    Results for larger data set and code for confusion matrices

--- BRRR/EEG_test.R
+++ BRRR/EEG_test.R
@@ -1,5 +1,7 @@
 setwd("/projects/FABEEG/BRRR")
 library("penalizedLDA")
+library("ggplot2")
+library("cvms")
 
 #every value of the variance parameter of Ω can
 #be immediately interpreted as the percentage of variance explained by the noise model as compared to the covariates
@@ -15,6 +17,8 @@ loadfile <- paste0("data/",ex,"spectrum.RData")
 #extract the age groups
 ages = read.csv('data/age_df.csv')
 ages <- ages[,-1]
+
+use_all=TRUE #perform LOO-CV or use all data in training?
 
 
 # Check if the file exists
@@ -32,6 +36,8 @@ if(file.exists(loadfile)) {
   A <- vector("list",length=S)
   names(A) <- subjects
   
+  corrupted = c()
+  
   # Saving the data into matrix A
   for(s in names(Y)) {
     
@@ -41,16 +47,15 @@ if(file.exists(loadfile)) {
       if(any(is.na(tmp))) browser()
       A[[s]] <- tmp[frequencies,chs]
       
-      corrupted=NULL
       
     } else {
-      corrupted = s
+      corrupted = c(corrupted, s)
     } 
   }
   
-  corrupted = c("FLE13539", "FLE141089") #this is bubblegum solution to remove singular groups
+  #corrupted = c("FLE13539", "FLE141089") #this is bubblegum solution to remove singular groups
   
-  if(!is.null(corrupted)){
+  if(length(corrupted)>0){
     obs <- subjects[subjects %in% corrupted == FALSE]
     A = A[names(A) %in% corrupted == FALSE] 
     ages = ages[ages$File %in% corrupted == FALSE,]
@@ -98,7 +103,6 @@ Y <- scale(Y, attr(tmp,"scaled:center"), attr(tmp,"scaled:scale"))
 
 # Saving classes to x
 x <- rep(NA,S); names(x) <- obs
-
 for(s in 1:S){
   x[obs[s]] <- ages[ages$File==obs[s],"Age.group"]
 }
@@ -107,8 +111,8 @@ for(s in 1:S){
 # identifier values +-1 is created
 # TODO: write warnings for case with singular groups!
 source("brrr.R")
-X <- matrix(0,S,M,dimnames=list(obs,paste0("class",3:(M+2)))) #NOTE! shifted because 2 first age groups removed
-for(i in 1:length(x)) if(!is.na(x[i])) X[i,x[i]-2] <- 1
+X <- matrix(0,S,M,dimnames=list(obs,paste0("class",1:M))) 
+for(i in 1:length(x)) if(!is.na(x[i])) X[i,x[i]] <- 1
 if(M==2) {
   print("Condensing two classes into one +-1 covariate.")
   X <- X[,1,drop=FALSE] 
@@ -128,10 +132,10 @@ looCV <- function(X, Y, model){
     print(paste0("CV ",testidx,"/",nrow(X)))
     
     if(model=="brrr"){
-      res <- brrr(X=X[-testidx,], Y=Y[-testidx,], K=3, n.iter=100, thin=5, fam=x[-testidx]) #res==mcmc.output
+      res <- brrr(X=X[-testidx,], Y=Y[-testidx,], K=3, n.iter=500, thin=5, fam=x[-testidx]) #res==mcmc.output
       pred[testidx,] <- X[testidx,,drop=F]%*%res$model$brr$context$Psi%*%res$model$brr$context$Gamma
       
-      lat_space=Y%*%ginv(averageGamma(res))
+      lat_space=Y%*%ginv(averageGamma(res)) #only makes sense to return this when trained with all data?
       
       for(m in 1:M){
         D[testidx,m] <- mean(abs(lat_space[testidx,]-res$model$brr$context$Psi[m,]))
@@ -162,38 +166,83 @@ looCV <- function(X, Y, model){
 
 ######## BRRRR #########
 
-pred <- X*NA
-res <- brrr(X=X,Y=Y, K=3,n.iter=500,thin=5,init="LDA", fam = x) #fit the model
-res$scaling <- ginv(averageGamma(res))
-W <- res$scaling
-
-pred <- X%*%res$model$brr$context$Psi%*%res$model$brr$context$Gamma #X%*%Psi%*%Gamma
-
-
-results <- looCV(X=X, Y=Y, model="brrr") #perform LOO-CV
-distmat <- results[[2]]
-PROJ <- distmat*0
-
-for(r in 1:nrow(distmat)){ #assign age groups based on lat. space distances
-  index <- which.min(distmat[r,])
-  PROJ[r,index] <- 1
+if(use_all==TRUE){
+  pred <- X*NA
+  res <- brrr(X=X,Y=Y, K=3,n.iter=500,thin=5,init="LDA", fam = x) #fit the model
+  res$scaling <- ginv(averageGamma(res))
+  W <- res$scaling
+  lat_space=Y%*%W
+  
+  pred <- X%*%res$model$brr$context$Psi%*%res$model$brr$context$Gamma #X%*%Psi%*%Gamma
+  D <- matrix(NA, nrow(X), ncol(X), dimnames=list(names(x), c())) #distance matrix
+  
+  for(testidx in 1:nrow(lat_space)){
+    for(m in 1:M){
+      D[testidx,m] <- mean(abs(lat_space[testidx,]-res$model$brr$context$Psi[m,])) 
+    }
+  }
+  
+  
+  
+  PROJ <- D*0
+  
+  for(r in 1:nrow(D)){ #assign age groups based on lat. space distances
+    index <- which.min(D[r,])
+    PROJ[r,index] <- 1
+  }
+  colnames(PROJ) <- c(paste0("class",1:M))
+  
+  #convert model matrices to factors and plot confusion matrix
+  P_factor <- as.factor(colnames(PROJ))[PROJ %*% 1:ncol(PROJ)]
+  X_factor <- as.factor(colnames(X))[X %*% 1:ncol(X)]
+  
+  
+  #results are somewhat catastrophic
+  png("figures/full_LOO_confmat_89.png")
+  cmat = confusion_matrix(X_factor, P_factor)
+  plot_confusion_matrix(cmat$`Confusion Matrix`[[1]])
+  dev.off()  
+  
+  
+  heatmap(lat_space) # lat_space; these two should be the same
+  heatmap(X%*%res$model$brr$context$Psi)
+  #however, they are absolutely NOT
+  
+} else if(use_all=FALSE){
+  results <- looCV(X=X, Y=Y, model="brrr") #perform LOO-CV
+  distmat <- results[[2]]
+  PROJ <- distmat*0
+  
+  for(r in 1:nrow(distmat)){ #assign age groups based on lat. space distances
+    index <- which.min(distmat[r,])
+    PROJ[r,index] <- 1
+  }
+  colnames(PROJ) <- c(paste0("class",1:M))
+  
+  #convert model matrices to factors and plot confusion matrix
+  P_factor <- as.factor(colnames(PROJ))[PROJ %*% 1:ncol(PROJ)]
+  X_factor <- as.factor(colnames(X))[X %*% 1:ncol(X)]
+  
+  
+  #results are somewhat catastrophic
+  png("figures/LOO_confmat_89.png")
+  cmat = confusion_matrix(X_factor, P_factor)
+  plot_confusion_matrix(cmat$`Confusion Matrix`[[1]])
+  dev.off()
 }
-colnames(PROJ) <- c(paste0("class",3:(M+2)))
 
-#TODO: construct a confusion matrix
-#results are somewhat catastrophic
+
+
+
 
 heatmap(Y)
-heatmap(Yhat)
+heatmap(results[[1]])
 heatmap(Y%*%W) #ONLY WHEN TRAINED WITH FULL DATA
 heatmap(Yhat%*%W)
 
 
 ######### LDA ###########
 
-
-
-Ydot <- looCV(X=X, Y=Y, model="penlda")
 
 
 
