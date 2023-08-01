21dc15bc3147c836bc890507785ddc94c0f64335
heikkiv7 <verna.heikkinen@aalto.fi>
2023-08-01 11:43:11 +0300

    update upper frequency limit

--- python-pipelines/eeg/runAll.sh
+++ python-pipelines/eeg/runAll.sh
@@ -3,7 +3,8 @@
 
 while read LINE; do
     #echo ${LINE}
-    #python3 psds_03.py $LINE
+    python3 freqfilt_01.py $LINE
+    python3 psds_03.py $LINE
     python3 bandpower_04.py $LINE 
 done < EEG_subjects.txt
 
