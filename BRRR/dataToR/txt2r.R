5ceb4fc2c081038c4146c570e58cd7dd3e181eaf
heikkiv7 <verna.heikkinen@aalto.fi>
2023-08-02 16:35:49 +0300

    updated data reading logic

--- BRRR/dataToR/txt2r.R
+++ BRRR/dataToR/txt2r.R
@@ -2,7 +2,7 @@
 setwd("/projects/FABEEG")
 
 set.seed(121)
-savepath <- paste0(getwd(), "/Data2R/absolute_spectra_1min/") #folder that contains spectra
+savepath <- paste0(getwd(), "/Data2R/relative_spectra_1min/") #folder that contains spectra
 types <- list.files(savepath) #gives the number of groups/families/individuals
 
 #for testing purposes, let us work only with a subset of individuals
@@ -23,21 +23,21 @@ for(t in 1:length(types)) {
   samples <- rep(NA,length(files))
   
   for(j in 1:length(files)) {
-    file <- files[1]
+    file <- files[2]
     #takes MEG matrices per subject for temporary storage
     X <- t(read.table(paste0(datapath,files[j]),sep=",",header=F))
-    subj <- file
-    subj <- sub("N1a.csv", "", subj)
+    subj <- type
+    #subj <- sub("N2c.csv", "", subj)
     Y[[subj]] <- X #*1e11
     #Y[[length(Y)+1]] <- X*1e11
-    individuals[[type]] <- c(individuals[[type]], subj) #Old naming
+    individuals[[type]] <- file #Old naming
   }
   
 }
-subjects <- unlist(individuals)
+subjects <- names(individuals)
 names(Y) <- subjects
-freq <- read.csv(paste0(getwd(),"BRRR/data/f.txt"),header=F)
-fname <- paste0(getwd(), "/data/new_N1Aspectrum.RData") #name whichever spectra you may use 
+freq <- read.csv(paste0(getwd(),"/BRRR/data/f.txt"),header=F)
+fname <- paste0(getwd(), "/BRRR/data/new_N1Bspectrum.RData") #name whichever spectra you may use 
 save(Y, subjects, individuals, freq, file=fname) #saving data
 
 
