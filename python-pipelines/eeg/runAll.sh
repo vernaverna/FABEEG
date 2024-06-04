2752ee873ba2daa450f94a734096d857d4c215ea
heikkiv7 <verna.heikkinen@aalto.fi>
2024-06-04 16:34:16 +0300

    new runs

--- python-pipelines/eeg/runAll.sh
+++ python-pipelines/eeg/runAll.sh
@@ -3,8 +3,8 @@
 
 while read LINE; do
     #echo ${LINE}
-    #python3 psds_03.py $LINE 
-    python3 PSD_04b.py $LINE 
+    python3 psds_03.py $LINE 
+    #python3 PSD_04b.py $LINE 
 done < EEG_subjects.txt
 
 
